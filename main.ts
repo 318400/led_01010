55935365094ed0a3941e76e37c7069c3018f256e
318400 <91246976+318400@users.noreply.github.com>
2021-11-25 16:09:54 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -45,17 +45,19 @@ input.onButtonPressed(Button.A, function () {
         }
     }
     basic.pause(100)
+    basic.clearScreen()
 })
 input.onButtonPressed(Button.B, function () {
-    let e = 0
-    let d = 0
-    let c = 0
-    let b = 0
-    let a: null = null
     basic.clearScreen()
+    if (true) {
+        a = 255
+        b = 150
+        c = 100
+        d = 50
+        e = 10
+    }
     x = 0
     y = 0
-    x = 0
     list = [
     [
     a,
@@ -93,12 +95,28 @@ input.onButtonPressed(Button.B, function () {
     e
     ]
     ]
-    for (let y = 0; y <= 4; y++) {
-        for (let x = 0; x <= 4; x++) {
-            led.plotBrightness(x, y, list[y][x])
+    basic.pause(100)
+    for (let index = 0; index < 5; index++) {
+        a += a
+        b += b
+        c += c
+        d += d
+        e += e
+        for (let y = 0; y <= 4; y++) {
+            for (let x = 0; x <= 4; x++) {
+                led.plotBrightness(x, y, list[y][x])
+            }
         }
+        basic.pause(100)
     }
+    basic.pause(500)
+    basic.clearScreen()
 })
+let e = 0
+let d = 0
+let c = 0
+let b = 0
+let a = 0
 let list: number[][] = []
 let y = 0
 let x = 0
@@ -162,3 +180,28 @@ for (let y = 0; y <= 4; y++) {
 }
 basic.pause(500)
 basic.clearScreen()
+if (a >= 250) {
+    a += a - 50
+} else if (a <= 10) {
+    a += a + 50
+}
+if (b >= 250) {
+    b += b - 50
+} else if (b <= 10) {
+    b += b + 50
+}
+if (c >= 250) {
+    c += c - 50
+} else if (c <= 10) {
+    c += c + 50
+}
+if (d >= 250) {
+    d += d - 50
+} else if (d <= 10) {
+    d += d + 50
+}
+if (e >= 250) {
+    e += e - 50
+} else if (e <= 10) {
+    e += e + 50
+}
